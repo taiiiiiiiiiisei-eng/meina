fcbe3094aa0c81c0110f03de145d71e9350d19bc
taiiiiiiiiiisei-eng <taiiiiiiiiiisei@gmail.com>
2026-09-15 14:17:32 +0900

    Replace startup script for V4

--- start_meina.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-cd /d "%~dp0"
-if exist ".venv\Scripts\activate.bat" call ".venv\Scripts\activate.bat"
-python upgrade_meina.py
-python upgrade_meina_v3.py
-python upgrade_meina_twitch.py
-python run_meina.py
-pause
