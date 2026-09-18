cd9861888dc773dcd336292a94400106c8196d21
taiiiiiiiiiisei-eng <taiiiiiiiiiisei@gmail.com>
2026-09-18 17:20:23 +0900

    refactor: use shared launcher environment

--- start_meina.bat
+++ start_meina.bat
@@ -1,16 +1,8 @@
 @echo off
 cd /d "%~dp0"
 
-set "MEINA_PYTHON="
-
-if exist "%~dp0.venv_new\Scripts\python.exe" set "MEINA_PYTHON=%~dp0.venv_new\Scripts\python.exe"
-if not defined MEINA_PYTHON if exist "%~dp0.venv\Scripts\python.exe" set "MEINA_PYTHON=%~dp0.venv\Scripts\python.exe"
-
-if not defined MEINA_PYTHON (
-    echo.
-    echo めいなのPython環境が見つかりません。
-    echo 候補: %~dp0.venv_new\Scripts\python.exe / %~dp0.venv\Scripts\python.exe
-    echo.
+call "%~dp0meina_env.bat"
+if errorlevel 1 (
     pause
     exit /b 1
 )
